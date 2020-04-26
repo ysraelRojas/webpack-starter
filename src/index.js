92c755c280a57c1cce4c94bd1793ee36291bf9f9
Ysrael <yrra_rojas@hotmail.com>
2020-04-26 01:08:09 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { saludar } from './js/componentes';
+import './styles.css';
+
+saludar('Ysrael');
---
+++ webpack.prod.js
@@ -0,0 +1,74 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+    mode: 'production',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsWebpackPlugin()]
+    },
+    output: {
+        filename: 'main.[contentHash].js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            }, {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contentHash].css',
+            ignoreOrder: false
+        }),
+        new CopyWebpackPlugin([{
+            from: 'src/assets',
+            to: 'assets/'
+        }]),
+        new MinifyPlugin(),
+        new CleanWebpackPlugin()
+    ]
+};
